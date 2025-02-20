421d0f1a970d112e4cf7ab98d3d8aebdbd219e04
ITWeiss <gottfried98@bk.ru>
2025-02-20 20:09:29 +0700

    Controllers

---
+++ src/main/java/com/example/controller/config/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package com.example.controller.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
